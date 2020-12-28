ac5b06f1b89d1bd073222f6fbbf016038be830d1
Danielle Jenkins <git@d6e.io>
2020-12-27 23:01:18 -0800

    Pass websocket midi data to midi virtual port.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+#[macro_use]
+extern crate lazy_static;
 mod midi;
 
+use midir::MidiOutputConnection;
+use regex::Regex;
+use std::borrow::BorrowMut;
 use std::net::SocketAddr;
 use std::thread;
 use websocket::sync::Server;
 use websocket::OwnedMessage;
 
 fn main() {
-    match midi::run() {
-        Ok(_) => ws_server(),
-        Err(err) => println!("Error: {}", err),
-    }
+    ws_server()
+}
+
+lazy_static! {
+    static ref PARSE_MIDI: Regex =
+        Regex::new(r"^(\[?P<first>\d{1,3},)(?P<second>\d{0,3},)(?P<third>\d{0,3}])").unwrap();
 }
 
 fn ws_server() {
@@ -22,6 +29,8 @@ fn ws_server() {
             let ip = client.peer_addr().unwrap();
 
             println!("Connection from {}", ip);
+
+            let mut midi_conn = midi::connect().unwrap();
 
             let (mut receiver, mut sender) = client.split().unwrap();
 
@@ -40,6 +49,25 @@ fn ws_server() {
                     }
                     OwnedMessage::Text(msg) => {
                         println!("Message: {}", msg);
+                        let msg = msg.trim();
+                        if msg.starts_with("[") {
+                            let midi: [u8; 3] = PARSE_MIDI
+                                .captures(msg)
+                                .map(|x| {
+                                    [
+                                        x.name("first").unwrap().as_str().parse::<u8>().unwrap(),
+                                        x.name("second").unwrap().as_str().parse::<u8>().unwrap(),
+                                        x.name("third").unwrap().as_str().parse::<u8>().unwrap(),
+                                    ]
+                                })
+                                .unwrap();
+                            midi_conn.send(&midi);
+                        } else {
+                            println!(
+                                "Message not valid midi: {} \nMust be in format [0,0,0].",
+                                msg
+                            );
+                        }
                     }
                     OwnedMessage::Binary(data) => {
                         println!("Binary: {:?}", data);
--- src/midi.rs
+++ src/midi.rs
@@ -1,64 +1,44 @@
+extern crate midir;
 
-use std::io::{stdin, stdout, Write};
 use std::error::Error;
+use std::io::{stdin, stdout, Write};
+use std::thread::sleep;
+use std::time::Duration;
 
-use midir::{MidiInput, MidiOutput, MidiIO, Ignore};
+use self::midir::MidiOutputConnection;
+use midir::{MidiOutput, MidiOutputPort};
 
-fn main() {
-    match run() {
-        Ok(_) => (),
-        Err(err) => println!("Error: {}", err)
-    }
-}
+pub(crate) fn connect() -> Result<MidiOutputConnection, Box<dyn Error>> {
+    let midi_out = MidiOutput::new("WS Midi Out")?;
 
-#[cfg(not(target_arch = "wasm32"))] // conn_out is not `Send` in Web MIDI, which means it cannot be passed to connect
-pub(crate) fn run() -> Result<(), Box<dyn Error>> {
-    let mut midi_in = MidiInput::new("midir forwarding input")?;
-    midi_in.ignore(Ignore::None);
-    let midi_out = MidiOutput::new("midir forwarding output")?;
+    // Get an output port (read from console if multiple are available)
+    let out_ports = midi_out.ports();
+    let out_port: &MidiOutputPort = match out_ports.len() {
+        0 => return Err("no output port found".into()),
+        1 => {
+            println!(
+                "Choosing the only available output port: {}",
+                midi_out.port_name(&out_ports[0]).unwrap()
+            );
+            &out_ports[0]
+        }
+        _ => {
+            println!("\nAvailable output ports:");
+            for (i, p) in out_ports.iter().enumerate() {
+                println!("{}: {}", i, midi_out.port_name(p).unwrap());
+            }
+            print!("Please select output port: ");
+            stdout().flush()?;
+            let mut input = String::new();
+            stdin().read_line(&mut input)?;
+            out_ports
+                .get(input.trim().parse::<usize>()?)
+                .ok_or("invalid output port selected")?
+        }
+    };
 
-    let in_port = select_port(&midi_in, "input")?;
-    println!();
-    let out_port = select_port(&midi_out, "output")?;
-
-    println!("\nOpening connections");
-    let in_port_name = midi_in.port_name(&in_port)?;
-    let out_port_name = midi_out.port_name(&out_port)?;
-
-    let mut conn_out = midi_out.connect(&out_port, "midir-forward")?;
-
-    // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
-    let _conn_in = midi_in.connect(&in_port, "midir-forward", move |stamp, message, _| {
-        conn_out.send(message).unwrap_or_else(|_| println!("Error when forwarding message ..."));
-        println!("{}: {:?} (len = {})", stamp, message, message.len());
-    }, ())?;
-
-    println!("Connections open, forwarding from '{}' to '{}' (press enter to exit) ...", in_port_name, out_port_name);
-
-    let mut input = String::new();
-    stdin().read_line(&mut input)?; // wait for next enter key press
-
-    println!("Closing connections");
-    Ok(())
-}
-
-fn select_port<T: MidiIO>(midi_io: &T, descr: &str) -> Result<T::Port, Box<dyn Error>> {
-    println!("Available {} ports:", descr);
-    let midi_ports = midi_io.ports();
-    for (i, p) in midi_ports.iter().enumerate() {
-        println!("{}: {}", i, midi_io.port_name(p)?);
-    }
-    print!("Please select {} port: ", descr);
-    stdout().flush()?;
-    let mut input = String::new();
-    stdin().read_line(&mut input)?;
-    let port = midi_ports.get(input.trim().parse::<usize>()?)
-        .ok_or("Invalid port number")?;
-    Ok(port.clone())
-}
-
-#[cfg(target_arch = "wasm32")]
-fn run() -> Result<(), Box<dyn Error>> {
-    println!("test_forward cannot run on Web MIDI");
-    Ok(())
+    println!("\nOpening connection");
+    let conn_out = midi_out.connect(out_port, "d6e-midi-port")?;
+    println!("Connection open. Listen for midi messages!");
+    Ok(conn_out)
 }
