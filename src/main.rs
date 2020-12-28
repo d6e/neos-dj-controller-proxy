92edaaeddf91461c6c8ec76ce5472e582d867234
Danielle Jenkins <git@d6e.io>
2020-12-28 00:29:43 -0800

    Replace multi-threading
    It's unnecessarily complex for the intended purpose.

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
 
 lazy_static! {
     static ref PARSE_MIDI: Regex =
-        Regex::new(r"^(\[?P<first>\d{1,3},)(?P<second>\d{0,3},)(?P<third>\d{0,3}])").unwrap();
+        Regex::new(r"^\[(?P<first>\d{1,3}),(?P<second>\d{0,3}),(?P<third>\d{0,3})\]$").unwrap();
 }
 
 fn ws_server() {
@@ -24,59 +24,57 @@ fn ws_server() {
     let server = Server::bind(addr0).unwrap();
     for request in server.filter_map(Result::ok) {
         // Spawn a new thread for each connection.
-        thread::spawn(|| {
-            let mut client = request.accept().unwrap();
-            let ip = client.peer_addr().unwrap();
+        let mut client = request.accept().unwrap();
+        let ip = client.peer_addr().unwrap();
 
-            println!("Connection from {}", ip);
+        println!("Connection from {}", ip);
 
-            let mut midi_conn = midi::connect().unwrap();
+        let mut midi_conn = midi::connect().unwrap();
 
-            let (mut receiver, mut sender) = client.split().unwrap();
+        let (mut receiver, mut sender) = client.split().unwrap();
 
-            for message in receiver.incoming_messages() {
-                let message = message.unwrap();
+        for message in receiver.incoming_messages() {
+            let message = message.unwrap();
 
-                match message {
-                    OwnedMessage::Close(_) => {
-                        let message = OwnedMessage::Close(None);
-                        sender.send_message(&message).unwrap();
-                        println!("Client {} disconnected", ip);
-                        return;
-                    }
-                    OwnedMessage::Ping(ping) => {
-                        println!("Ping: {:?}", ping);
-                    }
-                    OwnedMessage::Text(msg) => {
-                        println!("Message: {}", msg);
-                        let msg = msg.trim();
-                        if msg.starts_with("[") {
-                            let midi: [u8; 3] = PARSE_MIDI
-                                .captures(msg)
-                                .map(|x| {
-                                    [
-                                        x.name("first").unwrap().as_str().parse::<u8>().unwrap(),
-                                        x.name("second").unwrap().as_str().parse::<u8>().unwrap(),
-                                        x.name("third").unwrap().as_str().parse::<u8>().unwrap(),
-                                    ]
-                                })
-                                .unwrap();
-                            midi_conn.send(&midi);
-                        } else {
-                            println!(
-                                "Message not valid midi: {} \nMust be in format [0,0,0].",
-                                msg
-                            );
-                        }
-                    }
-                    OwnedMessage::Binary(data) => {
-                        println!("Binary: {:?}", data);
-                    }
-                    OwnedMessage::Pong(data) => {
-                        println!("Pong: {:?}", data);
-                    } // _ => sender.send_message(&message).unwrap(),
+            match message {
+                OwnedMessage::Close(_) => {
+                    let message = OwnedMessage::Close(None);
+                    sender.send_message(&message).unwrap();
+                    println!("Client {} disconnected", ip);
+                    return;
                 }
+                OwnedMessage::Ping(ping) => {
+                    println!("Ping={:?}", ping);
+                }
+                OwnedMessage::Text(msg) => {
+                    println!("Message: {}", msg);
+                    let msg = msg.trim();
+                    if msg.starts_with("[") {
+                        let midi: [u8; 3] = PARSE_MIDI
+                            .captures(msg)
+                            .map(|x| {
+                                [
+                                    x.name("first").unwrap().as_str().parse::<u8>().unwrap(),
+                                    x.name("second").unwrap().as_str().parse::<u8>().unwrap(),
+                                    x.name("third").unwrap().as_str().parse::<u8>().unwrap(),
+                                ]
+                            })
+                            .unwrap();
+                        midi_conn.send(&midi);
+                    } else {
+                        println!(
+                            "Message not valid midi: {} \nMust be in format [0,0,0].",
+                            msg
+                        );
+                    }
+                }
+                OwnedMessage::Binary(data) => {
+                    println!("Binary: {:?}", data);
+                }
+                OwnedMessage::Pong(data) => {
+                    println!("Pong: {:?}", data);
+                } // _ => sender.send_message(&message).unwrap(),
             }
-        });
+        }
     }
 }
